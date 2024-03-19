78cddbd3b12abaf4f69d047ce81a6cdb08b7831a
derbysanderos <derbydoank609@gmail.com>
2024-03-19 20:12:03 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas1</title>
+</head>
+<body>
+    <center>
+        <h1 >Biodata Mahasiswa</h1>
+        <style>
+        table, th, td{
+            border: 1px solid black;
+        }
+    </style>
+    <table style="width: 70%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Hobi</th>
+        </tr>
+        <tr>
+            <td>215150401111004</td>
+            <td>Derby Hadriyan Illiniy</td>
+            <td>Jl. Raya Blega Bangkalan</td>
+            <td>Volly, Billiard, Game</td>
+        </tr>
+    </table>
+    </center>
+    
+</body>
+</html>
